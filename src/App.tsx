5405e957476ffc22d533b28c4c6842164583504f
User <user@MacBook-Air-User.local>
2024-08-06 20:57:37 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,27 @@ import reactLogo from './assets/react.svg'
 import twaLogo from './assets/tapps.png'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { useSwipeable } from 'react-swipeable'
 import WebApp from '@twa-dev/sdk'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const images = [twaLogo, viteLogo, reactLogo]
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length),
+    onSwipedRight: () => setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true
+  })
+
+  WebApp.MainButton.show()
 
   return (
-    <>
-      <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div {...handlers} className="swipe-container">
+      <img src={images[currentIndex]} className="logo" alt="Swipeable Logo" />
       <h1>TWA + Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      {/*  */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
-    </>
+    </div>
   )
 }
 
